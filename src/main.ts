123ca7dfb722c2fdd0017670b718c07113f131c0
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-07-26 19:58:43 -0300

    feat: add fastify on bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { FastifyAdapter, type NestFastifyApplication } from '@nestjs/platform-fastify'
 
 async function bootstrap (): Promise<void> {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  try {
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+    await app.listen(3000)
+  } catch (error) {
+    console.log(error)
+  }
 }
 void bootstrap()
