f29d2f30f9ba1ff4757fce9135f6ecf0c2a66f6f
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-07-26 21:17:44 -0300

    chore: add EnvConfigService in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { FastifyAdapter, type NestFastifyApplication } from '@nestjs/platform-fastify'
+import { EnvConfigService } from './infra/config/env-config/env-config.service'
 
 async function bootstrap (): Promise<void> {
   try {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
-    await app.listen(3000)
+    const envConfigService = app.get(EnvConfigService)
+    await app.listen(envConfigService.getAppPort())
   } catch (error) {
     console.log(error)
   }
