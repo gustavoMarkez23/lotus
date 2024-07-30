a208c9e6262e00de88424def49008a21e3c469f5
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-07-30 08:28:03 -0300

    test: ensures throws when descricao is not provided

---
+++ src/application/use-cases/marca/__tests__/unit/create-marca.spec.ts
@@ -0,0 +1,26 @@
+import { type MarcaOutput } from '@/application/dto/marca/marca-output'
+import { BadRequestError } from '@/application/errors/bad-request-error'
+import { MarcaEntity } from '@/domain/entities/marca.entity'
+import { mockMarcaProps } from '@/domain/mocks/mock-marca'
+import { type UseCase } from '@/domain/use-cases/shared/use-case'
+
+export type InputCreateMarca = {
+  descricao: string
+}
+export type OutputCreateMarca = MarcaOutput
+export class CreateMarca implements UseCase<InputCreateMarca, OutputCreateMarca> {
+  async execute (input: InputCreateMarca): Promise<OutputCreateMarca> {
+    const { descricao } = input
+    if (!descricao) throw new BadRequestError('Input data not providade')
+    return await Promise.resolve(new MarcaEntity(mockMarcaProps({}), 1))
+  }
+}
+
+describe('CreateMarca', () => {
+  test('Should throws error when descricao not provided', async () => {
+    const sut = new CreateMarca()
+    const props = Object.assign(mockMarcaProps({}), { descricao: null })
+    const promise = sut.execute(props)
+    await expect(promise).rejects.toThrow()
+  })
+})
