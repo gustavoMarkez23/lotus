ed15997cc4af751f0fe5476de3ae1ce8639cea37
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-07-31 18:07:44 -0300

    test: ensures throw if createEntityRepository thorws

--- src/application/use-cases/marca/__tests__/unit/create-marca.spec.ts
+++ src/application/use-cases/marca/__tests__/unit/create-marca.spec.ts
@@ -6,22 +6,27 @@ import { MarcaEntity, type MarcaProps } from '@/domain/entities/marca.entity'
 import { CreateEntityRepositoryStub } from '@/application/mocks/mock-marca'
 
 describe('CreateMarca', () => {
-  let props: MarcaProps
+  let mockProps: MarcaProps
   let createEntityRepositoryStub: CreateEntityRepository<MarcaEntity>
   let sut: CreateMarca
   beforeEach(() => {
-    props = mockMarcaProps({})
+    mockProps = mockMarcaProps({})
     createEntityRepositoryStub = new CreateEntityRepositoryStub()
     sut = new CreateMarca(createEntityRepositoryStub)
   })
   test('Should throws error when descricao not provided', async () => {
-    props = Object.assign(mockMarcaProps({}), { descricao: null })
-    const promise = sut.execute(props)
+    mockProps = Object.assign(mockMarcaProps({}), { descricao: null })
+    const promise = sut.execute(mockProps)
     await expect(promise).rejects.toThrow(new BadRequestError('Input data not providade'))
   })
   test('Should call createEntityRepository with correct params', async () => {
     const createSpy = jest.spyOn(createEntityRepositoryStub, 'create')
-    await sut.execute(props)
-    expect(createSpy).toHaveBeenCalledWith(new MarcaEntity(props))
+    await sut.execute(mockProps)
+    expect(createSpy).toHaveBeenCalledWith(new MarcaEntity(mockProps))
+  })
+  test('Should throw if createEntityRepository throws', async () => {
+    jest.spyOn(createEntityRepositoryStub, 'create').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.execute(mockProps)
+    await expect(promise).rejects.toThrow()
   })
 })
