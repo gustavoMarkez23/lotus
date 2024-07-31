f4cd95670874747b15290a721a0643bdb4bf9a94
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-07-31 09:45:56 -0300

    test: ensure calls createEntityRepository with correct params

--- src/application/use-cases/marca/__tests__/unit/create-marca.spec.ts
+++ src/application/use-cases/marca/__tests__/unit/create-marca.spec.ts
@@ -1,12 +1,31 @@
 import { mockMarcaProps } from '@/domain/mocks/mock-marca'
 import { CreateMarca } from '../../create-marca'
 import { BadRequestError } from '@/application/errors/bad-request-error'
+import { type CreateEntityRepository } from '@/application/protocols/shared/create-entity-repository'
+import { MarcaEntity, type MarcaProps } from '@/domain/entities/marca.entity'
 
+class CreateEntityRepositoryStub implements CreateEntityRepository<MarcaEntity> {
+  async create (entity: MarcaEntity): Promise<MarcaEntity> {
+    return await Promise.resolve(entity)
+  }
+}
 describe('CreateMarca', () => {
+  let props: MarcaProps
+  let createEntityRepositoryStub: CreateEntityRepository<MarcaEntity>
+  let sut: CreateMarca
+  beforeEach(() => {
+    props = mockMarcaProps({})
+    createEntityRepositoryStub = new CreateEntityRepositoryStub()
+    sut = new CreateMarca(createEntityRepositoryStub)
+  })
   test('Should throws error when descricao not provided', async () => {
-    const sut = new CreateMarca()
-    const props = Object.assign(mockMarcaProps({}), { descricao: null })
+    props = Object.assign(mockMarcaProps({}), { descricao: null })
     const promise = sut.execute(props)
     await expect(promise).rejects.toThrow(new BadRequestError('Input data not providade'))
+  })
+  test('Should call createEntityRepository with correct params', async () => {
+    const createSpy = jest.spyOn(createEntityRepositoryStub, 'create')
+    await sut.execute(props)
+    expect(createSpy).toHaveBeenCalledWith(new MarcaEntity(props))
   })
 })
--- src/application/use-cases/marca/create-marca.ts
+++ src/application/use-cases/marca/create-marca.ts
@@ -1,17 +1,20 @@
-import { type MarcaOutput } from '@/application/dto/marca/marca-output'
+import { MarcaOutputMapper, type MarcaOutput } from '@/application/dto/marca/marca-output'
 import { BadRequestError } from '@/application/errors/bad-request-error'
 import { MarcaEntity } from '@/domain/entities/marca.entity'
-import { mockMarcaProps } from '@/domain/mocks/mock-marca'
-import { type UseCase } from '@/domain/use-cases/shared/use-case'
+import { type UseCase } from '@/application/use-cases/shared/use-case'
+import { type CreateEntityRepository } from '@/application/protocols/shared/create-entity-repository'
 
 export type InputCreateMarca = {
   descricao: string
 }
 export type OutputCreateMarca = MarcaOutput
 export class CreateMarca implements UseCase<InputCreateMarca, OutputCreateMarca> {
+  constructor (private readonly createEntityRepository: CreateEntityRepository<MarcaEntity>) {}
   async execute (input: InputCreateMarca): Promise<OutputCreateMarca> {
     const { descricao } = input
     if (!descricao) throw new BadRequestError('Input data not providade')
-    return await Promise.resolve(new MarcaEntity(mockMarcaProps({}), 1))
+    const marca = new MarcaEntity(input)
+    await this.createEntityRepository.create(marca)
+    return MarcaOutputMapper(marca)
   }
 }
