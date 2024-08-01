2dcd02dd7fd87d56caea8a5be16fb2cd30b6caa3
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-08-01 08:51:55 -0300

    feat: add prisma service e module

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { EnvConfigModule } from '../config/env-config/env-config.module'
+import { ConfigService } from '@nestjs/config'
+import { PrismaService } from './prisma/prisma.service'
+
+@Module({
+  imports: [EnvConfigModule.forRoot()],
+  providers: [ConfigService, PrismaService],
+  exports: [PrismaService]
+})
+export class DatabaseModule {
+}
