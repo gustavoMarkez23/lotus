e8d35bbed4607e5b06e5af5ea440a5a464b68958
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-08-03 16:24:14 -0300

    test: ensures throw if GetMarcaRepository throws

--- src/application/use-cases/marca/__tests__/unit/get-marca.spec.ts
+++ src/application/use-cases/marca/__tests__/unit/get-marca.spec.ts
@@ -3,6 +3,7 @@ import { type GetMarcaRepositiry } from '@/application/protocols/marca/get-marca
 import { type UseCase } from '@/application/use-cases/shared/use-case'
 import { MarcaEntity } from '@/domain/entities/marca.entity'
 import { mockMarcaProps } from '@/domain/mocks/mock-marca'
+import { throwError } from '@/domain/mocks/mock-shared'
 import { faker } from '@faker-js/faker'
 
 class GetMarcaRepositiryStub implements GetMarcaRepositiry {
@@ -22,12 +23,22 @@ class GetMarca implements UseCase<InputGetMarca, OutputGetMarca> {
   }
 }
 describe('GetMarca', () => {
+  let getMarcaRepositiryStub: GetMarcaRepositiryStub
+  let sut: GetMarca
+  let id: number
+  beforeEach(() => {
+    id = faker.number.int()
+    getMarcaRepositiryStub = new GetMarcaRepositiryStub()
+    sut = new GetMarca(getMarcaRepositiryStub)
+  })
   test('Should call GetMarcaRepository with correctly values', async () => {
-    const getMarcaRepositiryStub = new GetMarcaRepositiryStub()
-    const sut = new GetMarca(getMarcaRepositiryStub)
-    const id = faker.number.int()
     const spyGet = jest.spyOn(getMarcaRepositiryStub, 'get')
     await sut.execute({ id })
     expect(spyGet).toHaveBeenCalledWith({ id })
+  })
+  test('Should throw if GetMarcaRepository throws', async () => {
+    jest.spyOn(getMarcaRepositiryStub, 'get').mockImplementationOnce(throwError)
+    const promise = sut.execute({ id })
+    await expect(promise).rejects.toThrow()
   })
 })
