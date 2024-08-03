400bc1d23aac369a37ad29ded1b83fe441b531b5
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-08-03 15:27:35 -0300

    test: ensure throw an error when creating a marca with invalid descricao

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/**'
+    '<rootDir>/src/**/*.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
