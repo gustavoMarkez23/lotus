e5a67043e6da3c47958c4917f295f5e4e5766f64
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-08-08 18:19:30 -0300

    feat: add search metodo

--- src/infra/database/prisma/marca/marca-prisma-repository.ts
+++ src/infra/database/prisma/marca/marca-prisma-repository.ts
@@ -1,14 +1,18 @@
 import { type CreateMarcaRepository } from '@/application/protocols/marca/create-marca-repository'
 import { type PrismaService } from '@/infra/database/prisma/prisma.service'
 import { type MarcaEntity } from '@/domain/entities/marca.entity'
-import { type InputCreateMarca } from '@/application/use-cases/marca/create-marca'
+import { type InputCreateMarca } from '@/application/usecases/marca/create-marca'
 import { marcaModelMapper } from './marca-model-mapper'
 import { type GetMarcaRepository } from '@/application/protocols/marca/get-marca-repository'
-import { type InputGetMarca } from '@/application/use-cases/marca/get-marca'
+import { type InputGetMarca } from '@/application/usecases/marca/get-marca'
 import { NotFoundError } from '@/domain/errors/not-found-error'
+import { type SearchMarcaParams, type SearchMarcaRepository, SearchMarcaResult } from '@/application/protocols/marca/search-marca-repository'
+import { type SearchResult } from '@/application/protocols/shared/searchable-repository'
 
-export class MarcaPrismaRepository implements CreateMarcaRepository, GetMarcaRepository {
+export class MarcaPrismaRepository implements CreateMarcaRepository, GetMarcaRepository, SearchMarcaRepository {
   constructor (private readonly prismaService: PrismaService) {}
+  filtableFields: string[] = ['descricao']
+  sortableFields: string[] = ['descricao', 'createdAt']
   async create (data: InputCreateMarca): Promise<MarcaEntity> {
     const marcaModel = await this.prismaService.marca.create({ data })
     return marcaModelMapper(marcaModel)
@@ -21,5 +25,51 @@ export class MarcaPrismaRepository implements CreateMarcaRepository, GetMarcaRep
     } catch (error) {
       throw new NotFoundError(`MarcaModel not found using ID ${data.id}`)
     }
+  }
+
+  async search (props: SearchMarcaParams): Promise<SearchResult<MarcaEntity, string>> {
+    const { page, perPage, sort, sortDir } = props
+    const filtable = this.filtableFields?.includes(String(props.filterField)) || false
+    const filter = filtable ? String(props.filter) : null
+    const filterField = filtable ? String(props.filterField) : null
+
+    const sortable = this.sortableFields?.includes(String(sort)) || false
+    const orderByField = sortable ? sort : 'createdAt'
+    const orderByDir = sortable ? sortDir : 'desc'
+
+    const take = props.perPage && props.perPage > 0 ? props.perPage : 15
+    const skip = props.page && props.page > 0 ? (props.page - 1) * take : 1
+
+    const count = await this.prismaService.marca.count({
+      ...(filter && {
+        where: {
+          [String(filterField)]: {
+            contains: filter
+          }
+        }
+      })
+    })
+    const models = await this.prismaService.marca.findMany({
+      ...(filter && {
+        where: {
+          [String(filterField)]: {
+            contains: filter
+          }
+        }
+      }),
+      orderBy: { [String(orderByField)]: String(orderByDir) },
+      skip,
+      take
+    })
+
+    return new SearchMarcaResult({
+      items: models.map(o => marcaModelMapper(o)),
+      total: count,
+      currentPage: page,
+      filter,
+      perPage,
+      sort,
+      sortDir
+    })
   }
 }
