bb9b7733ab00e81643da35f1c40c3d100efaaded
Gustavo Marques da Silva <gustavo.markez23@gmail.com>
2024-08-10 12:40:14 -0300

    refactor: move ActivateMarcaUsecase to own file

--- src/application/usecases/marca/__tests__/unit/activate-marca.spec.ts
+++ src/application/usecases/marca/__tests__/unit/activate-marca.spec.ts
@@ -1,27 +1,13 @@
-import { type InputGetMarca } from '@/application/dto/marca/marca-input'
-import { MarcaOutputMapper, type MarcaOutput } from '@/application/dto/marca/marca-output'
 import { mockGetMarcaRepository, mockUpdateMarcaRepository } from '@/application/mocks/mock-marca'
 import { type GetMarcaRepository } from '@/application/protocols/marca/get-marca-repository'
 import { type UpdateMarcaRepository } from '@/application/protocols/marca/update-marca-repository'
 import { MarcaEntity } from '@/domain/entities/marca.entity'
 import { mockMarcaProps } from '@/domain/mocks/mock-marca'
 import { throwError } from '@/domain/mocks/mock-shared'
-import { type UseCase } from '@/domain/protocols/shared/usecase'
 import { faker } from '@faker-js/faker'
+import { ActivateMarcaUsecase, type InputActivateMarca } from '@/application/usecases/marca/activate-marca'
 
-type InputActivateMarca = InputGetMarca
-type OutputActivateMarca = MarcaOutput
-class ActivateMarcaUsecase implements UseCase<InputActivateMarca, OutputActivateMarca> {
-  constructor (private readonly getMarcaRepository: GetMarcaRepository, private readonly updateMarcaRepository: UpdateMarcaRepository) {}
-  async execute (input: InputGetMarca): Promise<MarcaOutput> {
-    const entity = await this.getMarcaRepository.findById(input.id)
-    entity.activate()
-    await this.updateMarcaRepository.update(entity)
-    return MarcaOutputMapper(entity)
-  }
-}
-
-describe('ActivateMarca', () => {
+describe('ActivateMarcaUsecase', () => {
   let mockInput: InputActivateMarca
   let getMarcaRepositoryStub: GetMarcaRepository
   let updateMarcaRepositoryStub: UpdateMarcaRepository
